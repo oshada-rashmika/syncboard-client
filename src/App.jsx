c5c9b19a4a19bcf5cdd9e879e1c36520236ae51c
Oshada Rashmika <oshadar.rodrigo@gmail.com>
2026-08-02 14:54:55 +0530

    Spacing fix

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <main>
       <h1>SyncBoard</h1>
-      <TaskCard title="Design login" assignee="Nimali" due="Friday" done={false} />
+      <TaskCard title="Design login" assignee="Nimali" due="Friday" done={false} /><br />
       <TaskCard title="Fix Bugs" assignee="John Doe" due="Monday" done={true} />
     </main>
   )
